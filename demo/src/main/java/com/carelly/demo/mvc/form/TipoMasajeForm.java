41c3971c8eaeea2bdbab70fb1442d8cac55b3acc
matias <matias@matias.com>
2021-11-04 19:45:23 -0300

    Form para tipo de masajes OK

---
+++ demo/src/main/java/com/carelly/demo/mvc/form/TipoMasajeForm.java
@@ -0,0 +1,15 @@
+package com.carelly.demo.mvc.form;
+
+import com.carelly.demo.dto.TipoMasajeDto;
+
+public class TipoMasajeForm {
+    private TipoMasajeDto tipoMasaje;
+
+    public TipoMasajeDto getTipoMasaje() {
+        return tipoMasaje;
+    }
+
+    public void setTipoMasaje(TipoMasajeDto tipoMasaje) {
+        this.tipoMasaje = tipoMasaje;
+    }
+}
