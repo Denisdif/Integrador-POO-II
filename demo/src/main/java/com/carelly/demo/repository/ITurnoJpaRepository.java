b27ce9c26678d67ac4f56c801f34037a8fe94e28
Matías Benítez <matias.benitez.mab@gmail.com>
2021-11-17 18:46:46 -0300

    JPA para TURNOS ok

---
+++ demo/src/main/java/com/carelly/demo/repository/ITurnoJpaRepository.java
@@ -0,0 +1,17 @@
+package com.carelly.demo.repository;
+
+import java.util.List;
+
+import com.carelly.demo.model.Turno;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITurnoJpaRepository extends JpaRepository<Turno,Long>{
+    //select fields from Cliente where id='[parametro]'
+    Turno findByid(String id);
+    @Cacheable(value = "Turnos")
+    List<Turno> findAll();
+}
